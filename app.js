1e1f54688e3121589bfaf2bb0a2d082dbf6d8d6d
JCRMurillo <jc.reyes11@hotmail.com>
2025-09-04 23:11:58 +0100

    Agregar la funcion de sortear el nombre
    Se agrega la funcion para sortear el nombre y se hacen unas mejoras a la lista que se muestra

--- app.js
+++ app.js
@@ -21,8 +21,22 @@ function actualizarListaDeAmigos(){
     listaHTML.innerHTML = "";
 
     for (let amigo of listaDeAmigos) {
-        let elementoLista = document.createElement("a");
+        let elementoLista = document.createElement("div");
         elementoLista.textContent = amigo;
         listaHTML.appendChild(elementoLista);
     }
+}
+function sortearAmigo(){
+    let numeroDeAmigos = listaDeAmigos.length;
+    if (numeroDeAmigos === 0){
+        alert("no hay ningun nombre para ser sorteado");
+        return;
+    }
+    if (numeroDeAmigos <= 2){
+        alert("Necesita mas de dos nombres para hacer el sorteo");
+        return;
+    }
+    let indiceAleatorio = Math.floor(Math.random()*numeroDeAmigos);
+    let AmigoSorteado = document. getElementById("resultado").textContent = listaDeAmigos[indiceAleatorio];
+    
 }
