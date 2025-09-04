af8efdebaa238f51f47bbeeab9e76a6e665dc991
JCRMurillo <jc.reyes11@hotmail.com>
2025-09-04 22:35:16 +0100

    Primer paso / Agregar nombres a la lista

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
+let listaDeAmigos = [];
+function agregarAmigo(){
+    let nuevoAmigo = document.getElementById('amigo').value.trim();
+    if (nuevoAmigo === ""){
+        alert("Porfavor ingrese un nombre valido")
+        limpiarCaja();
+    } 
 
+    listaDeAmigos.push(nuevoAmigo);
+    actualizarListaDeAmigos();
+    limpiarCaja();
+    
+}
+
+function limpiarCaja(){
+    let valorCaja = document.getElementById("amigo").value = " ";
+}
+
+function actualizarListaDeAmigos(){
+    let listaHTML = document.getElementById("listaAmigos");
+    listaHTML.innerHTML = "";
+
+    for (let amigo of listaDeAmigos) {
+        let elementoLista = document.createElement("a");
+        elementoLista.textContent = amigo;
+        listaHTML.appendChild(elementoLista);
+    }
+}
